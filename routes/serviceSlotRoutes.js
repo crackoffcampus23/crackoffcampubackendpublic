53e7230746a854b10cd9f1d790bdd389208087e2
crackoffcampus23 <crackoffcampus23@gmail.com>
2025-12-17 09:10:48 +0000

    reupdating

--- routes/serviceSlotRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { verifySlot } = require('../controllers/serviceSlotController');
-
-const router = express.Router();
-
-// Public endpoint: POST /serviceverifier
-router.post('/serviceverifier', verifySlot);
-
-module.exports = router;
